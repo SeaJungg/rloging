0488d18b176e9ff650af12da3d66f282e5ab7fe3
ttochi <jjiny1127@gmail.com>
2022-11-14 00:23:55 +0900

    Apply semantic ui react

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,5 +1,107 @@
-function Home() {
-    return <div>Hi</div>;
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import {
+    Button,
+    Header,
+    Container,
+    Icon,
+    Menu,
+    Segment,
+    Sidebar,
+} from "semantic-ui-react";
+
+class MainSidebar extends Component {
+    state = {};
+
+    handleSidebarHide = () => this.setState({ sidebarOpened: false });
+
+    handleToggle = () => this.setState({ sidebarOpened: true });
+
+    render() {
+        const { children } = this.props;
+        const { sidebarOpened } = this.state;
+
+        return (
+            <Sidebar.Pushable>
+                <Sidebar
+                    as={Menu}
+                    animation="overlay"
+                    inverted
+                    onHide={this.handleSidebarHide}
+                    vertical
+                    visible={sidebarOpened}
+                >
+                    <Menu.Item as="a" active>
+                        Home
+                    </Menu.Item>
+                    <Menu.Item as="a">Log in</Menu.Item>
+                    <Menu.Item as="a">Sign Up</Menu.Item>
+                </Sidebar>
+
+                <Sidebar.Pusher dimmed={sidebarOpened}>
+                    <Segment
+                        id="home"
+                        inverted
+                        textAlign="center"
+                        style={{
+                            minHeight: "100vh",
+                            maxWidth: "820px",
+                            margin: "0 auto",
+                        }}
+                        vertical
+                    >
+                        <Container>
+                            <Menu inverted pointing secondary size="large">
+                                <Menu.Item onClick={this.handleToggle}>
+                                    <Icon name="sidebar" />
+                                </Menu.Item>
+                                <Menu.Item position="right">
+                                    <div>Hello Yujin</div>
+                                </Menu.Item>
+                            </Menu>
+                        </Container>
+                        {children}
+                    </Segment>
+                </Sidebar.Pusher>
+            </Sidebar.Pushable>
+        );
+    }
 }
 
-export default Home;
+const Homepage = () => (
+    <MainSidebar>
+        <Container>
+            <Header
+                as="h1"
+                inverted
+                style={{
+                    fontSize: "4em",
+                    fontWeight: "bolder",
+                    fontStyle: "italic",
+                    marginBottom: 0,
+                    marginTop: "3em",
+                }}
+            >
+                <span style={{ color: "#f2711c" }}>R</span>LOG
+            </Header>
+            <Header
+                as="h2"
+                content="RUN Every Thu. 8PM"
+                inverted
+                style={{
+                    fontSize: "1.5em",
+                    fontWeight: "normal",
+                    marginTop: "0.5em",
+                }}
+            />
+            <Link to="/login">
+                <Button color="orange" size="huge">
+                    Login
+                    <Icon name="right arrow" />
+                </Button>
+            </Link>
+        </Container>
+    </MainSidebar>
+);
+
+export default Homepage;
--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -1,5 +1,49 @@
-function Login() {
-    return <div>Login</div>;
+import { Button, Form, Grid, Header, Segment } from "semantic-ui-react";
+
+function LoginForm() {
+    return (
+        <Grid
+            textAlign="center"
+            style={{ height: "100vh" }}
+            verticalAlign="middle"
+        >
+            <Grid.Column style={{ maxWidth: 450 }}>
+                <Segment style={{ width: "90%", margin: "0 auto" }}>
+                    <Header as="h2" color="orange" textAlign="center">
+                        Log-in to your account
+                    </Header>
+                    <Form size="large">
+                        <Form.Input
+                            fluid
+                            icon="user"
+                            iconPosition="left"
+                            placeholder="이름+전화번호뒤4자리"
+                        />
+                        <Form.Input
+                            fluid
+                            icon="lock"
+                            iconPosition="left"
+                            placeholder="비밀번호"
+                            type="password"
+                        />
+
+                        <Button color="orange" fluid size="large">
+                            Login
+                        </Button>
+                    </Form>
+                    <a href="/signup">
+                        <Button
+                            fluid
+                            size="large"
+                            style={{ marginTop: "10px" }}
+                        >
+                            Sign Up
+                        </Button>
+                    </a>
+                </Segment>
+            </Grid.Column>
+        </Grid>
+    );
 }
 
-export default Login;
+export default LoginForm;
