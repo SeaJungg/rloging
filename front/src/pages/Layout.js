01f22fdc81b23164142b303c2eed78d417fbab7f
ttochi <jjiny1127@gmail.com>
2022-11-19 21:52:53 +0900

    Make Layout to function component

--- front/src/pages/Layout.js
+++ front/src/pages/Layout.js
@@ -1,61 +1,55 @@
-import React, { Component } from "react";
-import { Container, Icon, Menu, Segment, Sidebar } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Container, Menu, Segment, Sidebar, Icon } from "semantic-ui-react";
 
-class Layout extends Component {
-    state = {};
+function Layout({ children }) {
+    const [isOpen, setIsOpen] = useState(false);
 
-    handleSidebarHide = () => this.setState({ sidebarOpened: false });
+    return (
+        <Sidebar.Pushable>
+            <Sidebar
+                as={Menu}
+                animation="overlay"
+                inverted
+                onHide={() => setIsOpen(false)}
+                vertical
+                visible={isOpen}
+            >
+                <Link to="/home">
+                    <Menu.Item>Home</Menu.Item>
+                </Link>
+                <Link to="/home">
+                    <Menu.Item>Create Session</Menu.Item>
+                </Link>
+                <Menu.Item>Logout</Menu.Item>
+            </Sidebar>
 
-    handleToggle = () => this.setState({ sidebarOpened: true });
-
-    render() {
-        const { children } = this.props;
-        const { sidebarOpened } = this.state;
-
-        return (
-            <Sidebar.Pushable>
-                <Sidebar
-                    as={Menu}
-                    animation="overlay"
+            <Sidebar.Pusher dimmed={isOpen}>
+                <Segment
+                    id="home"
                     inverted
-                    onHide={this.handleSidebarHide}
+                    style={{
+                        minHeight: "100vh",
+                        maxWidth: "820px",
+                        margin: "0 auto",
+                    }}
                     vertical
-                    visible={sidebarOpened}
                 >
-                    <Menu.Item as="a" active>
-                        Home
-                    </Menu.Item>
-                    <Menu.Item as="a">Log in</Menu.Item>
-                    <Menu.Item as="a">Sign Up</Menu.Item>
-                </Sidebar>
-
-                <Sidebar.Pusher dimmed={sidebarOpened}>
-                    <Segment
-                        id="home"
-                        inverted
-                        style={{
-                            minHeight: "100vh",
-                            maxWidth: "820px",
-                            margin: "0 auto",
-                        }}
-                        vertical
-                    >
-                        <Container>
-                            <Menu inverted pointing secondary size="large">
-                                <Menu.Item onClick={this.handleToggle}>
-                                    <Icon name="sidebar" />
-                                </Menu.Item>
-                                <Menu.Item position="right">
-                                    <div>Hello Yujin</div>
-                                </Menu.Item>
-                            </Menu>
-                        </Container>
-                        {children}
-                    </Segment>
-                </Sidebar.Pusher>
-            </Sidebar.Pushable>
-        );
-    }
+                    <Container>
+                        <Menu inverted pointing secondary size="large">
+                            <Menu.Item onClick={() => setIsOpen(true)}>
+                                <Icon name="sidebar" />
+                            </Menu.Item>
+                            <Menu.Item position="right">
+                                <div>Hello Yujin</div>
+                            </Menu.Item>
+                        </Menu>
+                    </Container>
+                    {children}
+                </Segment>
+            </Sidebar.Pusher>
+        </Sidebar.Pushable>
+    );
 }
 
 export default Layout;
