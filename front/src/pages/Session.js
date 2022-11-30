be07378a129e4ecb3fb8bb25cb2886c4d0e70d8f
ttochi <jjiny1127@gmail.com>
2022-12-01 00:12:46 +0900

    Apply api fetch on session detail page

--- front/src/pages/Session.js
+++ front/src/pages/Session.js
@@ -1,31 +1,45 @@
-import Layout from "./Layout";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import { Button, Header, Segment, Statistic } from "semantic-ui-react";
+import Layout from "./Layout";
 import MainHeader from "../components/MainHeader";
-import { useState } from "react";
+import { getDateString } from "../utils";
 
 function Session() {
     const { id } = useParams();
-    const [attendee, setAttendee] = useState(Math.floor(Math.random() * 50));
+    const [sessionDetail, setSessionDetail] = useState();
+    const [attendee, setAttendee] = useState(0);
+    const [total, setTotal] = useState(0);
     const [isAttend, setIsAttend] = useState(false);
-    const total = 40;
+
+    const fetchSessionDetail = async (id) => {
+        // TODO: modify json format query
+        const res = await axios.get(`/api/v1/session/${id}/?format=json`);
+        setSessionDetail(res.data);
+        setAttendee(res.data?.attendee || 0);
+        setTotal(res.data?.total_attendee || 0);
+    };
+
+    useEffect(() => {
+        fetchSessionDetail(id);
+    }, [id]);
 
     return (
         <Layout>
-            <MainHeader content="Sessions Detail" />
-            <Header as="h3" inverted>
-                {id}
-            </Header>
+            <MainHeader content={sessionDetail?.name} />
             <Segment circular color="orange" inverted>
                 <Header as="h2">
                     <Header.Subheader>장소</Header.Subheader>
-                    서울색공원
+                    {sessionDetail?.place || "미정"}
                 </Header>
             </Segment>
             <Segment circular>
                 <Header as="h2">
                     <Header.Subheader inverted>집결시간</Header.Subheader>
-                    2022-11-10 19:50
+                    {sessionDetail?.launch_date
+                        ? getDateString(sessionDetail.launch_date)
+                        : "미정"}
                 </Header>
             </Segment>
             <Segment style={{ overflow: "hidden" }}>
