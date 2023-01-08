ae65ca67434097219d23c256bbe07ce6cd4fe98c
ttochi <jjiny1127@gmail.com>
2023-01-08 21:46:28 +0900

    Disable github page and proxy to backend

--- front/src/config.js
+++ front/src/config.js
@@ -1,6 +1,5 @@
 const config = {};
 
-config.endpoint =
-    process.env.REACT_APP_ENDPOINT || "http://rlog-seoul.kr/api/v1";
+config.endpoint = process.env.REACT_APP_ENDPOINT || "/api/v1";
 
 export default config;
